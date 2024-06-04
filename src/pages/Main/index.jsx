418e373e24fbb54b863d2f88e1cf6a23145d0d20
Edward <47050768+piyalkinee@users.noreply.github.com>
2024-06-04 22:47:40 +0500

    Add main page

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -4,8 +4,51 @@ import './style-tablet.css'
 import './style-desktop.css'
 
 const MainPage = () => (
-    <div>
-        <p>Responsive Content</p>
+    <div className='main-page'>
+        <div className='main-page-welcome-block'>
+            <div className='main-page-welcome-block-b1'>
+                <h1>Работа в Казахстане</h1>
+                <h2>Работа для всех специальностей</h2>
+                <button className='main-page-button'>Список вакансий</button>
+            </div>
+            <div className='main-page-welcome-block-b2'>
+                <a className='main-page-for-applicant' href="/applicant">Для работадателей</a>
+            </div>
+        </div>
+        <div className='main-page-services'>
+            <div className='main-page-services-block'>
+                <h3>Особенность</h3>
+                <p>Текст</p>
+            </div>
+            <div className='main-page-services-block'>
+                <h3>Особенность</h3>
+                <p>Текст</p>
+            </div>
+            <div className='main-page-services-block'>
+                <h3>Особенность</h3>
+                <p>Текст</p>
+            </div>
+            <div className='main-page-services-block'>
+                <h3>Особенность</h3>
+                <p>Текст</p>
+            </div>
+            <div className='main-page-services-block'>
+                <h3>Особенность</h3>
+                <p>Текст</p>
+            </div>
+            <div className='main-page-services-block'>
+                <h3>Особенность</h3>
+                <p>Текст</p>
+            </div>
+        </div>
+        <div className='main-page-about-us'>
+            <div className='main-page-about-us-block-b1'>
+                <button className='main-page-button'>Мы в телеграм</button>
+            </div>
+            <div className='main-page-about-us-block-b2'>
+                <p className='main-page-info' href="/applicant">© Вакансии 2024</p>
+            </div>
+        </div>
     </div>
 );
 
